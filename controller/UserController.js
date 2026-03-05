84dac1f4195d8c910fde1de6cf4015506aa154be
gowthamanniit <karurgowthaman@gmail.com>
2026-03-05 08:46:41 +0530

    added files

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,18 +1,19 @@
 import User from '../model/UserModel.js'
 export const login = async(req,res) =>{
     try {
-          //const  newUser = new User(req.body);
+          const  newUser = new User(req.body);
+          const {username,password}=newUser;
           //const  {email} = newUser;
-          const {username,password}=req.body;
-          const un = await User.findOne({username});
+         // const {username,password}=req.body;
+          const un = await User.findOne({username,password});
           if(!un) {
                    return res.status(400).json({message : "User not found!!!"});
           }
-          const pw = await User.findOne({password});
+          //const pw = await User.findOne({password});
           
-          if(!pw) {
-            return res.status(400).json({message : "Invalid credentials!!!"});
-   }
+          //if(!pw) {
+            //return res.status(400).json({message : "Invalid credentials!!!"});
+   //}
      //      const savedData = await newUser.save();
      //      res.status(200).json(savedData);
              res.status(200).json({message:"login successful"});
